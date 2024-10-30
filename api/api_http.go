cc239578f8f5f6022b18663482c5ffabe8fd545d
Yee <6344844@gmail.com>
2024-10-30 13:33:03 +0800

    update:添加resty httpclient，增加xhs_debug环境变量

--- api/api_http.go
+++ api/api_http.go
@@ -2,10 +2,14 @@ package api
 
 import (
 	"errors"
-	"github.com/valyala/fasthttp"
 	"net"
 	"net/http"
+	"os"
+	"sync"
 	"time"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/valyala/fasthttp"
 )
 
 const HttpTTL = 1 * time.Minute
@@ -29,27 +33,82 @@ func CreateFastHttpClient() fasthttp.Client {
 	}
 }
 
+var restyClient *resty.Client
+var _once sync.Once
+
+func Transport200() *http.Transport {
+	return &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          1000,             //针对整个Client的所有idle池中的连接数的和
+		MaxIdleConnsPerHost:   20,               //在client进入闲时时要进入idle状态的最大连接数。
+		MaxConnsPerHost:       100,              //某个host建立最大连接数的设置
+		IdleConnTimeout:       60 * time.Second, //IdleConnTimeout字段用于超时清理idle池中的长连接
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+}
+
+func GetRestyClient() *resty.Client {
+	_once.Do(func() {
+		restyClient = resty.New()
+		restyClient.SetTimeout(time.Millisecond * 1500)
+		restyClient.SetRetryCount(1)
+		restyClient.SetRetryWaitTime(time.Millisecond * 100)
+		restyClient.SetRetryMaxWaitTime(time.Second)
+		restyClient.SetDebug(os.Getenv("xhs_debug") == "true")
+		restyClient.SetTransport(Transport200())
+	})
+	return restyClient
+}
+
+func PostWithRestyClient(req []byte) ([]byte, error) {
+
+	cli := GetRestyClient()
+	cli.SetHeader("Content-Type", "application/json")
+	resp, err := cli.R().SetBody(req).Post(ApiUrl)
+	if err != nil {
+		return nil, err
+	}
+	if resp == nil || resp.StatusCode() != http.StatusOK {
+		return nil, errors.New("http resp is nil")
+	}
+	if len(resp.Body()) == 0 { // 避免 json.Unmarshal 出现 unexpected end of JSON input 错误
+		return nil, errors.New("http resp body is nil")
+	}
+
+	return resp.Body(), nil
+}
+
 func Post(req []byte) ([]byte, error) {
-	var resp []byte
-	httpReq := fasthttp.AcquireRequest()
-	defer fasthttp.ReleaseRequest(httpReq)
+	return PostWithRestyClient(req)
+	/*
+		var resp []byte
+		httpReq := fasthttp.AcquireRequest()
+		defer fasthttp.ReleaseRequest(httpReq)
 
-	httpResp := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseResponse(httpResp)
+		httpResp := fasthttp.AcquireResponse()
+		defer fasthttp.ReleaseResponse(httpResp)
 
-	httpReq.SetRequestURI(ApiUrl)
-	httpReq.Header.SetContentType("application/json;charset=utf-8")
-	httpReq.SetBody(req)
-	httpReq.Header.SetMethod(http.MethodPost)
+		httpReq.SetRequestURI(ApiUrl)
+		httpReq.Header.SetContentType("application/json;charset=utf-8")
+		httpReq.SetBody(req)
+		httpReq.Header.SetMethod(http.MethodPost)
 
-	if err := FastClient.DoTimeout(httpReq, httpResp, HttpTTL); err != nil {
-		return resp, err
-	}
+		if err := FastClient.DoTimeout(httpReq, httpResp, HttpTTL); err != nil {
+			return resp, err
+		}
 
-	resp = httpResp.Body()
-	if len(resp) == 0 { // 避免 json.Unmarshal 出现 unexpected end of JSON input 错误
-		return resp, errors.New("http resp body is nil")
-	}
+		resp = httpResp.Body()
+		if len(resp) == 0 { // 避免 json.Unmarshal 出现 unexpected end of JSON input 错误
+			return resp, errors.New("http resp body is nil")
+		}
 
-	return resp, nil
+		return resp, nil
+		*
+	*/
 }
--- go.mod
+++ go.mod
@@ -3,21 +3,29 @@ module github.com/mryee2023/xhs-sdk
 go 1.18
 
 require (
+	github.com/bytedance/sonic v1.12.1
+	github.com/go-resty/resty/v2 v2.15.3
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/valyala/fasthttp v1.26.0
-	github.com/bytedance/sonic v1.12.1
+	github.com/zsmhub/xhs-sdk v1.0.5
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/bytedance/sonic/loader v0.2.0 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/klauspost/compress v1.12.2 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
-	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
