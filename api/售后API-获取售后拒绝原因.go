7d71a4c47b967f1605c48983dd934e285c428033
Yee <6344844@gmail.com>
2024-11-01 10:24:39 +0800

    add:afterSale.rejectReasons获取售后拒绝原因

---
+++ api/售后API-获取售后拒绝原因.go
@@ -0,0 +1,45 @@
+package api
+
+import json "github.com/bytedance/sonic"
+
+type (
+	ReqAfterSaleRejectReasons struct {
+		BaseRequest
+		ReturnsId string `json:"returnsId"`
+		//拒绝原因类型 1：审核拒绝 2：收货拒绝
+		RejectReasonType int64 `json:"rejectReasonType"`
+	}
+)
+
+var _ Request = new(ReqAfterSaleRejectReasons)
+
+func (r ReqAfterSaleRejectReasons) Method() string {
+	return "afterSale.rejectReasons"
+}
+
+func (r ReqAfterSaleRejectReasons) Params(base BaseRequest) []byte {
+	r.BaseRequest = base
+	ret, _ := json.Marshal(r)
+	return ret
+}
+
+type (
+	RespAfterSaleRejectReasons struct {
+		BaseResponse
+	}
+)
+
+var _ Response = new(RespAfterSaleRejectReasons)
+
+func (r RespAfterSaleRejectReasons) ErrorCode() int64 {
+	return r.BaseResponse.ErrorCode
+}
+
+func (r RespAfterSaleRejectReasons) ErrorMsg() string {
+	return r.BaseResponse.ErrorMsg
+
+}
+
+func (r RespAfterSaleRejectReasons) Success() bool {
+	return r.BaseResponse.Success
+}
